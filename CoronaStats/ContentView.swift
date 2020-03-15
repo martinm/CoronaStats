bd85273c0f3ff33256a3bf6d3d085b8a070ed6c5
martinm <malinowskimn@gmail.com>
2020-03-15 11:35:24 +0000

    creating new project

---
+++ CoronaStats/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoronaStats
+//
+//  Created by Martin Malinowski on 15/03/2020.
+//  Copyright © 2020 Martin Malinowski. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
